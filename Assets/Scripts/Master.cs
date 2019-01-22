cc0d551ff2128921aaffa8ed3bd701245b9b1019
Eben Blaisdell <emblaisdell@foresthills.net>
2019-01-21 23:25:03 -0500

    add infinite planets

--- Assets/Scripts/Master.cs
+++ Assets/Scripts/Master.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Master : MonoBehaviour
 {
     public GameObject planet;
+    public Transform spaceship;
+
+    public int points;
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,31 +4,44 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    public static List<GameObject> planets = new List<GameObject>();
-    public static float planetDist = 1000;
-    public static float errorRatio = 0.1f;
-    public static int numPlanets = 10;
+    public Master master;
+
+    public const float planetDist = 1000;
+    public const float errorRatio = 0.1f;
+    public const int numPlanets = 10;
+    public const float destroyDistance = 1000;
 
     public static Vector3 lastPlanetPos = Vector3.zero;
 
     public static Vector3 creationDirection = Vector3.forward;
 
-    public static float hitMult;
+    public static float atmoMult = 1.1f;
     float radius;
-    bool hitYet = false;
+
+    private Vector3 direction;
     
     void Start()
     {
         radius = 150 + 250 * Random.value;
         transform.localScale = new Vector3(radius, radius, radius);
+        direction = creationDirection;
+
+        master = FindObjectOfType<Master>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(Spaceship.spaceship.position, transform.position) <= hitMult * radius)
+        if (Vector3.Dot(Spaceship.spaceship.position - transform.position, direction) > destroyDistance)
         {
-
+            creationDirection = (creationDirection + errorRatio * Random.insideUnitSphere).normalized;
+            lastPlanetPos += planetDist * creationDirection;
+            Instantiate(master.planet, lastPlanetPos, Quaternion.identity, null);
+            Destroy(gameObject);
+        }
+        if (Vector3.Distance(Spaceship.spaceship.position, transform.position) <= atmoMult * radius)
+        {
+            print("hit");
         }
     }
 
