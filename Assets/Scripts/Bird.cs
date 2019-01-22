8b064c00563faa47d4f4c9acfd455a6b3a7e27e6
Eben Blaisdell <emblaisdell@foresthills.net>
2019-01-21 20:36:29 -0500

    Added Bird

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float turnSpeed;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate
+        transform.RotateAround(Vector3.up, turnSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+        transform.RotateAround(transform.right, turnSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+        // Move
+        transform.position += moveSpeed * Time.deltaTime * transform.forward;
+    }
+}
