e5b027d931da63f15c022660fc84fc95d37d9d36
Eben Blaisdell <emblaisdell@foresthills.net>
2019-01-21 22:48:33 -0500

    Basic planets

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public static List<GameObject> planets = new List<GameObject>();
+    public static float planetDist = 1000;
+    public static float errorRatio = 0.1f;
+    public static int numPlanets = 10;
+
+    public static Vector3 lastPlanetPos = Vector3.zero;
+
+    public static Vector3 creationDirection = Vector3.forward;
+
+    public static float hitMult;
+    float radius;
+    bool hitYet = false;
+    
+    void Start()
+    {
+        radius = 150 + 250 * Random.value;
+        transform.localScale = new Vector3(radius, radius, radius);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(Spaceship.spaceship.position, transform.position) <= hitMult * radius)
+        {
+
+        }
+    }
+
+    void GetHit()
+    {
+
+    }
+}
--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bird : MonoBehaviour
+public class Spaceship : MonoBehaviour
 {
+    public static Transform spaceship;
     public float turnSpeed;
     public float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spaceship = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Rotate
-        transform.RotateAround(Vector3.up, turnSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
-        transform.RotateAround(transform.right, turnSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        transform.Rotate(transform.up, turnSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+        transform.Rotate(transform.right, turnSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
 
         // Move
         transform.position += moveSpeed * Time.deltaTime * transform.forward;
